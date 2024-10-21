bc30fb933d17510e4c7af980fbe78586d11f8102
Anugrah <pnanugrah@gmail.com>
2024-10-21 01:11:09 -0700

    finished linux version and made README

--- lua/goto-github.lua
+++ lua/goto-github.lua
@@ -1,11 +1,40 @@
 local M = {}
 
-M.get_cursor_under_text = function ()
+M.get_cursor_under_text = function()
   return vim.fn.expand("<cfile>")
 end
 
-M.make_github_link = function (text)
-  local repo_owner
+M.get_owner = function(str)
+  local result = str:match("([^/]+)")
+  return result
+end
+
+M.get_repo = function(str)
+  local result = str:match("/(.+)")
+  return result
+end
+
+M.build_link = function(owner, repo)
+  local link = "https://www.github.com/" .. owner .. "/" .. repo
+  return link
+end
+
+M.open_link = function()
+  local text = M.get_cursor_under_text()
+  local owner = M.get_owner(text)
+  local repo = M.get_repo(text)
+  local link = M.build_link(owner, repo)
+
+  vim.fn.jobstart({ "xdg-open", link })
+  -- TODO: Test these on respective Operating Systems
+  -- vim.fn.jobstart({"open", link})   -- For macOS
+  -- vim.fn.jobstart({"start", link})  -- For Windows
+end
+
+M.setup = function()
+  vim.keymap.set("n", "<leader>go", function()
+    M.open_link()
+  end)
 end
 
 return M
